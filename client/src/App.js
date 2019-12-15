46559042988aa0066f9d864ab47978b9578f96b5
Ami <ami.nakano@outlook.com>
2019-12-15 22:08:36 +0100

    added worker

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Typography from "@material-ui/core/Typography";
 import Jobs from "./Jobs";
 
 const mockJobs = [
---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,28 @@
+var fetch = require("node-fetch");
+var redis = require('redis');
+var client = redis.createClient(); 
+const { promisify } = require("util");
+// const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.get).bind(client);
+
+const baseURL = "https://jobs.github.com/positions.json";
+
+async function fetchGithub() {
+	let resultCount = 1, onPage = 0;
+	const allJobs = [];
+
+	while(resultCount > 0) {
+		const res = await fetch(`${baseURL}?page=${onPage}`);
+		const jobs = await res.json();
+		allJobs.push(...jobs);
+		resultCount = jobs.length;
+		console.log("got ", resultCount, " jobs");
+		onPage++;
+	}
+	console.log("got ", allJobs.length, "jobs total");
+	const success = await setAsync("github", allJobs);
+	console.log({success});
+}
+fetchGithub(); 
+
+module.exports = fetchGithub;
