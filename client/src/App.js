787984c56348df1c764b7d65db84974f479d82c7
Ami <ami.nakano@outlook.com>
2019-12-16 18:04:03 +0100

    Start with create-react-app

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,8 @@ import React from 'react';
 import './App.css';
 import Jobs from "./Jobs";
 
-const JOB_API_URL = "http://localhost:3001/jobs";
-
-// const mockJobs = [
-//   { title: "SWE 1", company: "Google" },
-//   { title: "SWE 1", company: "Apple" },
-//   { title: "SWE 1", company: "Facebook" },
-// ];
+const JOB_API_URL = "/api/jobs";
+// const JOB_API_URL = "http://localhost:3001/jobs";
 
 async function fetchJobs(updateCb) {
   const res = await fetch(JOB_API_URL);
