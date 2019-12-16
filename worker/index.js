7f22091ff39e563d66d0f157bc98b822a82a796e
Ami <ami.nakano@outlook.com>
2019-12-16 11:44:06 +0100

    job title filter works

--- worker/index.js
+++ worker/index.js
@@ -2,4 +2,4 @@ var CronJob = require("cron").CronJob;
 
 const fetchGithub = require("./tasks/fetch-github");
 
-new CronJob("* * * * * *", fetchGithub, null, true, "America/Los_Angeles");
+new CronJob("* * * * *", fetchGithub, null, true, "America/Los_Angeles");
--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -3,14 +3,16 @@ var redis = require('redis');
 var client = redis.createClient(); 
 const { promisify } = require("util");
 // const getAsync = promisify(client.get).bind(client);
-const setAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
 const baseURL = "https://jobs.github.com/positions.json";
 
 async function fetchGithub() {
 	let resultCount = 1, onPage = 0;
 	const allJobs = [];
+	console.log("fetching github");
 
+	// fetch all pages
 	while(resultCount > 0) {
 		const res = await fetch(`${baseURL}?page=${onPage}`);
 		const jobs = await res.json();
@@ -20,9 +22,27 @@ async function fetchGithub() {
 		onPage++;
 	}
 	console.log("got ", allJobs.length, "jobs total");
-	const success = await setAsync("github", allJobs);
+
+	const jrJobs = allJobs.filter(job => {
+		const jobTitle = job.title.toLowerCase();
+
+		if (
+				jobTitle.includes("senior") || 
+				jobTitle.includes("manager") ||
+				jobTitle.includes("sr.") ||
+				jobTitle.includes("architect")
+			) {
+				return false;
+			} 			
+				return true;
+	})
+
+  console.log("filtered down to ", jrJobs.length, "jobs total");
+
+
+	const success = await setAsync("github", JSON.stringify(jrJobs));
 	console.log({success});
 }
-fetchGithub(); 
+// fetchGithub(); 
 
 module.exports = fetchGithub;
